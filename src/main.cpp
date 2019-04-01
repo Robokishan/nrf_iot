dcf7e4962f1143102c5f5a3333225affab6b9e2e
Kishan Joshi <robokishan.blogspot@gmail.com>
2019-04-01 10:12:55 +0530

    makefile final (mbedtls baki che)

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,17 @@
-#include "openthread.h"
+#include "ble.h"
+
+#include <assert.h>
+#include <openthread-core-config.h>
+#include <openthread/config.h>
+
+#include <openthread/diag.h>
+#include <openthread/ncp.h>
+#include <openthread/tasklet.h>
+
+#include "openthread-system.h"
 
 int main(void){
-    
+    otInstance *instance;
+
+    instance = otInstanceInitSingle();
 }
---
+++ third_party/openthread/openthread.mk
@@ -0,0 +1,7 @@
+INCLUDE_DIR = openthread/third_party/mbedtls/repo/include
+# C_DIRS += /home/kishan/Softwares/nrf_iot/third_party/openthread/openthread/src/ncp/
+C_DIRS += /home/kishan/Softwares/nrf_iot/third_party/openthread/openthread/src/core/
+
+# SRC_DIRS := $(shell find $(C_DIRS) -name *.cpp -or -name *.c -or -name *.s)
+CPP_FILES += $(shell find $(C_DIRS)  -name \*.cpp)
+SRC_DIRS += $(shell find $(C_DIRS)  -name \*.c)
